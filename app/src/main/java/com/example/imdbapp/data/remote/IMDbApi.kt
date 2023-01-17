17d806b230b19dd89939b3d3aa90b0684d243918
Maciej Pelc <78445032+Pelc314@users.noreply.github.com>
2023-01-18 00:39:33 +0100

    added rapidapi key

--- app/src/main/java/com/example/imdbapp/data/remote/IMDbApi.kt
+++ app/src/main/java/com/example/imdbapp/data/remote/IMDbApi.kt
@@ -1,12 +1,16 @@
 package com.example.imdbapp.data.remote
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface IMDbApi {
     @GET("actors/list-most-popular-celebs")
-    suspend fun callMostPopularCelebs(): List<String>
+    suspend fun callMostPopularCelebs(
+        @Query("rapidapi-key") apiKey: String = API_KEY
+    ): List<String>
 
     companion object {
         const val BASE_URL = "https://imdb8.p.rapidapi.com/"
+        const val API_KEY = "https://imdb8.p.rapidapi.com/"
     }
 }
