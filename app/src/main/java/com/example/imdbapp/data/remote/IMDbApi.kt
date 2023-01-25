c6a2b9c2c6a476a46b14fd3404dd0b092417722b
Maciej Pelc <78445032+Pelc314@users.noreply.github.com>
2023-01-25 10:06:30 +0100

    hidden apikey (it is still visible in previous commits obviously)

--- app/src/main/java/com/example/imdbapp/data/remote/IMDbApi.kt
+++ app/src/main/java/com/example/imdbapp/data/remote/IMDbApi.kt
@@ -1,6 +1,6 @@
 package com.example.imdbapp.data.remote
 
-import com.example.imdbapp.core.Constants.API_KEY
+import com.example.imdbapp.core.ApiKey
 import com.example.imdbapp.data.remote.dto.TopMovieDto
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,11 +8,11 @@ import retrofit2.http.Query
 interface IMDbApi {
     @GET("actors/list-most-popular-celebs")
     suspend fun callMostPopularCelebs(
-        @Query("rapidapi-key") apiKey: String = API_KEY
+        @Query("rapidapi-key") apiKey: String = ApiKey.API_KEY
     ): List<String>
 
     @GET("title/get-top-rated-movies")
     suspend fun getTopRatedMovies(
-        @Query("rapidapi-key") apiKey: String = API_KEY
+        @Query("rapidapi-key") apiKey: String = ApiKey.API_KEY
     ): List<TopMovieDto>
 }
